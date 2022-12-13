cc1b6887e60f5bcf4039260d2623abaf3a873d96
i-moonlight <infinite.moonlight0506@gmail.com>
2022-12-13 15:09:57 -0300

    Initial commit

---
+++ back-end/Program.cs
@@ -0,0 +1,51 @@
+/*
+    necessary packages to this project:
+        - Microsoft.EntityFrameworkCore -v 7.0.0
+        - Microsoft.EntityFrameworkCore.Tools -v 7.0.0
+        - Microsoft.EntityFrameworkCore.Relational -v 7.0.0
+        - Pomelo.EntityFrameworkCore.MySql -v 6.0.2
+        - AutoMapper -v 12.0.0
+        - AutoMapper.Extensions.Microsoft.DependencyInjection -v 12.0.0
+        - Microsoft.AspNetCore.Mvc.NewtonsoftJson -v 6.0.10 <IN CASE IT'S NECESSARY TO USE HTTP PATCH INSTEAD OF PUT>
+
+    necessary tools for this project:
+        - dotnet-ef -v 7.0.0
+
+    Console commands:
+        - dotnet add package Microsoft.EntityFrameworkCore -v 6.0.7
+        - dotnet add package Microsoft.EntityFrameworkCore.Tools -v 6.0.7
+        - dotnet add package Microsoft.EntityFrameworkCore.Relational -v 6.0.7
+        - dotnet add package Pomelo.EntityFrameworkCore.MySql -v 6.0.2
+        - dotnet add package AutoMapper -v 12.0.0
+        - dotnet add package AutoMapper.Extensions.Microsoft.DependencyInjection -v 12.0.0
+        - dotnet add package Microsoft.AspNetCore.Mvc.NewtonsoftJson -v 6.0.10
+        - dotnet tool install --global dotnet-ef -v 7.0.0
+        - dotnet ef migrations add <NomeDaMigration>
+        - dotnet ef database update
+*/
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
