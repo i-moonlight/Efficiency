3852d261cf84a1fdaf55b46f192c0490b54dda30
i-moonlight <infinite.moonlight0506@gmail.com>
2022-12-27 17:13:58 -0300

    installed uikit for the front-end; created home component and initial routing with lazy loading

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "",
+        pathMatch: "full",
+        loadChildren: () =>
+            import("./Pages/home/home.module").then((m) => m.HomeModule),
+    },
+];
+
+/**
+ * To create new components with routing enabled and configured automatically
+ * use something like:
+ * ng generate module <component> --route <component> --module app.module
+ **/
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: [
+        "./app.component.css",
+        "../../node_modules/uikit/dist/css/uikit.css",
+    ],
 })
 export class AppComponent {
-  title = 'front-end';
+    title = "front-end";
 }
